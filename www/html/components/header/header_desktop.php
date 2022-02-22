cbe1e124f8c8853775c19adba3fd90ca1e187c09
Hakkı <hakki.ceylan2356@gmail.com>
2022-02-22 21:29:54 +0300

    new header design

--- www/html/components/header/header_desktop.php
+++ www/html/components/header/header_desktop.php
@@ -1,41 +1,46 @@
 <?php 
-  if(isset($_SESSION['user'])) {
+if(isset($_SESSION['user'])) {
 
 ?>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light center">
-        <div class="navbar-inner"> 
-          <ul class="navbar-nav">
-              <li class="nav-item" id="suggestions">
-                  <a class="nav-link" href="suggestions.php">Tavsiye Ver</a>
-              </li>
-              <li class="nav-item" id="leaderboard">
-                  <a class="nav-link" href="leaderboard.php">Liderlik Tablosu</a>
-              </li>
-              <li class="nav-item" id="profile">
-                  <a class="nav-link" href="profile.php">Profil</a>
-              </li>
-              <li class="nav-item">
-                  <a class="nav-link" href="logout.php">Çıkış Yap</a>
-              </li>
-          </ul>
-        </div>
-      </nav>
-<?php
+    <nav class="navbar navbar-expand-lg navbar-light">
+        <div class="container-fluid container-sm justify-content-around">
+            <ul class="navbar-nav">
+                <a class="navbar-brand" href="#">
+                    <div class="d-flex flex-column">
+                        <img src="https://st2.depositphotos.com/1385144/11058/v/950/depositphotos_110586344-stock-illustration-dynamic-logo-letter-d-logo.jpg"
+                            alt="brand" width="64" height="64" class="d-inline-block align-text-top ml-2">
+                        <span><span class="text-white bg-dark">En</span> Doğrusu</span>
 
-  } else {
-?>
-      <nav class="navbar navbar-expand-lg navbar-light bg-light text-right">
-        <div class="navbar-inner"> 
-          <ul class="navbar-nav">
-              <li class="nav-item" id="login">
-                  <a class="nav-link" href="login.php">Giriş Yap</a>
-              </li>
-              <li class="nav-item" id="register">
-                  <a class="nav-link" href="register.php">Kayıt Ol</a>
-              </li>
-          </ul>
+                    </div>
+                </a>
+                <li class="nav-item mt-4" id="suggestions">
+                    <a class="nav-link" href="suggestions.php">Tavsiye Ver</a>
+                </li>
+                <li class="nav-item mt-4" id="leaderboard">
+                    <a class="nav-link" href="leaderboard.php">Liderlik Tablosu</a>
+                </li>
+                <li class="nav-item mt-4" id="profile">
+                    <a class="nav-link" href="profile.php">Profil</a>
+                </li>
+                <li class="nav-item mt-4">
+                    <a class="nav-link" href="logout.php">Çıkış Yap</a>
+                </li>
+            </ul>
         </div>
-      </nav>
-<?php
-  }
-?>
+    </nav>
+
+<?php } else { ?>
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-light text-right">
+        <div class="navbar-inner">
+            <ul class="navbar-nav">
+                <li class="nav-item" id="login">
+                    <a class="nav-link" href="login.php">Giriş Yap</a>
+                </li>
+                <li class="nav-item" id="register">
+                    <a class="nav-link" href="register.php">Kayıt Ol</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+<?php } ?>
