f76c5c10b20f095d4eb37bd918d434a2a9bf0219
Hakkı <hakki.ceylan2356@gmail.com>
2022-03-01 18:59:54 +0300

    added permission_level

--- www/html/models/User.php
+++ www/html/models/User.php
@@ -23,6 +23,7 @@
             $this->password_hash = $arr['password_hash'] ?? null;
             $this->lockout = $arr['lockout'] ?? null;
             $this->point = $arr['point'] ?? 0;
+            $this->permission_level = $arr['permission_level'] ?? 0;
             $this->normalized_username = $arr['normalized_username'] ?? null;
             $this->normalized_email = $arr['normalized_email'] ?? null;
             if($isSession) $_SESSION["user"] = $this->toJSON();
@@ -30,7 +31,7 @@
 
         function toJSON(){
             $arr = [];
-            $arr["_id"]= $this->id ?? null;
+            $arr["_id"]= $this->id ?? new MongoDB\BSON\ObjectId(null);
             $arr["name"] = $this->name ?? null;
             $arr["username"] = $this->username ?? null;
             $arr["surname"] = $this->surname ?? null;
@@ -41,14 +42,15 @@
             $arr["normalized_username"] = $this->normalized_username ?? strtoupper($arr["username"]);
             $arr["normalized_email"] = $this->normalized_email ?? strtoupper($arr["email"]);
             $arr["point"] = $this->point ?? 0;
+            $arr["permission_level"] = $this->permission_level ?? 0;
             return $arr;
         }
 
-        function toJSONAsIdentitiy(){
+        function toJSONAsIdentitiy($with_password = True){
             $arr = [];
             if($this->email != null) 
                 $arr["email"] = $this->email;
-            if($this->password_hash != null) 
+            if($this->password_hash != null && $with_password) 
                 $arr["password_hash"] = $this->password_hash;
            return $arr;
         }
