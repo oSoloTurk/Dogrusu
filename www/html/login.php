b44774fd62ccfec8911b4c7b742f1922f6cd9183
Hakkı <hakki.ceylan2356@gmail.com>
2022-03-01 18:58:50 +0300

    reformated

--- www/html/login.php
+++ www/html/login.php
@@ -17,26 +17,26 @@ requiredLogin(false);
 
 <body>
 
-<?php 
-  require_once("header.php");
+    <?php
+    require_once("header.php");
 
-  require_once("models/User.php");
+    require_once("models/User.php");
 
-  if (isset($_POST['login'])) {
-    $mail = htmlspecialchars($_POST['email'], ENT_QUOTES, 'utf-8');
-    $pwd = hash('sha256', htmlspecialchars($_POST['password'], ENT_QUOTES, 'utf-8'));
-    $user = new User(["email" => $mail, "passwordhash" => $pwd]);
+    if (isset($_POST['login'])) {
+        $mail = htmlspecialchars($_POST['email'], ENT_QUOTES, 'utf-8');
+        $pwd = hash('sha256', htmlspecialchars($_POST['password'], ENT_QUOTES, 'utf-8'));
+        $user = new User(["email" => $mail, "passwordhash" => $pwd]);
 
-    $user->password_hash = $pwd;
-    $result = $db->users->findOne($user->toJSONAsIdentitiy());
+        $user->password_hash = $pwd;
+        $result = $db->users->findOne($user->toJSONAsIdentitiy());
 
-    if ($result != null) {
-      $user = new User($result, true);
-      sendToPage("index.php?msg=logged");
-    } else {
-      sendToPage("login.php?msg=wrong_input");
-    }
-  } ?>
+        if ($result != null) {
+            $user = new User($result, true);
+            sendToPage("index.php?msg=logged");
+        } else {
+            sendToPage("login.php?msg=wrong_input");
+        }
+    } ?>
 
     <article>
         <div class="row justify-content-center">
@@ -60,7 +60,7 @@ requiredLogin(false);
                                 <input type="checkbox" name="remember" id="remember">
                                 <label for="remember">Beni Hatırla</label>
                             </div>
-                        </div>                        
+                        </div>
                         <div class="mt-3">
                             <div class="row d-flex justify-content-between">
                                 <button class="btn btn-outline-primary ml-4" name="login" type="submit">Giriş
@@ -71,7 +71,8 @@ requiredLogin(false);
                         </div>
                         <hr />
                         <p class="text-justify text-center">
-                          <a class="clickable pretty " href="register.php">Henüz üye değilseniz kayıt olmak için tıklayın</a></p>
+                            <a class="clickable pretty " href="register.php">Henüz üye değilseniz kayıt olmak için tıklayın</a>
+                        </p>
                     </form>
                 </div>
             </div>
