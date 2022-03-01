8b0c9cdbb496417ea2f4d7e716f5858d74b5531d
Hakkı <hakki.ceylan2356@gmail.com>
2022-03-01 18:57:47 +0300

    created 404 page for admin pages

---
+++ www/html/admin/404.php
@@ -0,0 +1,12 @@
+<div class="row justify-content-center">
+    <div class="col-md-4">
+        <div class="mt-5 border">
+            <span class="mt-5 position-absolute text-center fs-1 w-100 h-25 pr-4">
+                <span style="font-size: xxx-large;">404</span>
+                </br>
+                Böyle bir sayfa yok..
+            </span>
+            <img class="w-100 h-100" src="../images/404.jpg">
+        </div>
+    </div>
+</div>
